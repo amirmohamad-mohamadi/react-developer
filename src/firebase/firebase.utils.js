52524d3ead4a31ce1a12b225c67bc64ff5713613
Amir Mohamad Mohamadi <amirmm5560@gmail.com>
2021-03-08 02:11:36 +0330

    finishing sign in handleSubmit method

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,14 +11,39 @@ const config = {
     appId: "1:865438357487:web:0f00572f6df4001bd4881a",
     measurementId: "G-T9FL59EPTX"
 };
+export const createUserProfileDocument = async (userAuth, addintioalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...addintioalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+};
 
 firebase.initializeApp(config);
 
- export const auth = firebase.auth();
- export const firestore = firebase.firestore();
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
 
- const provider = new firebase.auth.GoogleAuthProvider();
- provider.setCustomParameters({ prompt: 'select_account'});
- export const signInWithGoogle = () => auth.signInWithPopup(provider);
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
- export default firebase;
+export default firebase;
