dab9f16343cbe061f6e15fc10ce7d4022d2169e1
Amir Mohamad Mohamadi <amirmm5560@gmail.com>
2021-03-16 02:34:41 +0330

    update shop data with new data, update cart utils go group items in cart reducer

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -4,14 +4,14 @@ import CollectionItem from '../collection-item/collection-item.component';
 
 import './collection-preview.styles.scss';
 
-const CollectionPreview = ({title, items}) => (
+const CollectionPreview = ({ title, items }) => (
     <div className='collection-preview'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
             {items
                 .filter((item, idx) => idx < 4)
-                .map(({id, ...otheritemProps}) => (
-                    <CollectionItem key={id}{...otheritemProps} />))
+                .map(item => (
+                    <CollectionItem key={item.id} item={item} />))
             }
         </div>
     </div>
